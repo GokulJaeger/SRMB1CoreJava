8f059d3eda17fe09e42db5676863a7e6ee2a91c1
GokulJaeger <gokuljaeger@gmail.com>
2021-07-10 08:06:20 +0530

    day 16 assignment  updated

--- src/com/srm/cjava/week3/day16/TempConv.java
+++ src/com/srm/cjava/week3/day16/TempConv.java
@@ -1,78 +1,85 @@
 package com.srm.cjava.week3.day16;
 
-import java.text.DecimalFormat;
-
+import java.awt.*;          
+import java.awt.event.*;    
+import javax.swing.*;   
  
- 
-public class TempConv {
-
-	private static String temperatureScale;
-    double tempt;
- 
-		public TempConv() {
-
- 
-		tempt = 0.0;
-
- 
-		temperatureScale = "C";
-
-	}
- 
-	public TempConv(double temp) {
-
- 
-		tempt = temp;
-
-	}
- 
-	public static void useFahrenheit() {
-
-		temperatureScale = "F";
-	}
- 
-	public static void useCelsius() {
-
-		temperatureScale = "C";
-	}
- 
-	public static void useKelvin() {
-	
-		temperatureScale = "K";
-	}
- 
-	public void set(double value) {
-		if (temperatureScale == "C"){
-			tempt = value;
-		}
-		else if(temperatureScale == "F"){
-			tempt = (value - 32) * 5/9.0;
- 
-		}else if (temperatureScale == "K"){
-			tempt = (value - 273.15);
-		}
- 
-	}
- 
-	public String toString(){
-		String temperatureString;
- 
-		if(temperatureScale == "F"){
-			tempt = tempt * 9/5.0 + 32;
- 
-		}else if (temperatureScale == "K"){
-			tempt = tempt + 273.15;
-		}
-		DecimalFormat df = new DecimalFormat("#.##");
-		temperatureString = df.format(tempt) + '\u00b0' + temperatureScale;
- 
-		return temperatureString;
-	}
- 
-	public TempConv mean(TempConv otherTemperature) {
-		double average = (tempt + otherTemperature.tempt) /2.0;
-		TempConv mean = new TempConv (average);
- 
-		return mean;
-	}	
+public class TempConv extends JFrame
+{
+    private JLabel celsiusLabel;        
+    private JLabel fahrenheitLabel;
+     
+    private JTextField celsiusTF;       
+    private JTextField fahrenheitTF;    
+     
+    private CelsHandler celsiusHandler;
+    private FahrHandler fahrenheitHandler;
+     
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 75;   
+    private static final double FTOC = 5.0/9.0;
+    private static final double CTOF = 9.0/5.0;
+    private static final int OFFSET = 32;
+     
+    public TempConv()
+    {
+        setTitle("Temperature Conversion");
+        Container c = getContentPane();
+        c.setLayout(new GridLayout(1,4));
+         
+        celsiusLabel = new JLabel("Temp in Celsius: ",
+                                            SwingConstants.RIGHT);
+        fahrenheitLabel = new JLabel("Temp in Fahrenheit: ",
+                                            SwingConstants.RIGHT);
+                                             
+        celsiusTF = new JTextField(7);
+        fahrenheitTF = new JTextField(7);
+         
+        c.add(celsiusLabel);
+        c.add(celsiusTF);
+        c.add(fahrenheitLabel);
+        c.add(fahrenheitTF);
+         
+        celsiusHandler = new CelsHandler();
+        fahrenheitHandler = new FahrHandler();
+         
+        celsiusTF.addActionListener(celsiusHandler);
+        fahrenheitTF.addActionListener(fahrenheitHandler);
+         
+        setSize (WIDTH, HEIGHT);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+     
+    private class CelsHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            double celsius, fahrenheit;
+             
+            celsius =
+                    Double.parseDouble(celsiusTF.getText());
+            fahrenheit = celsius * CTOF + OFFSET;
+            fahrenheitTF.setText(String.format("%.2f", fahrenheit));
+        }
+    }
+     
+    private class FahrHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            double celsius, fahrenheit;
+            fahrenheit = 
+                    Double.parseDouble(fahrenheitTF.getText());
+            celsius = (fahrenheit - OFFSET) * FTOC;
+            celsiusTF.setText(String.format("%.2f", celsius));
+        }
+    }
+             
+     
+    public static void main(String[] args)
+    {
+        TempConv tempConv = new TempConv();
+        tempConv.setLocationRelativeTo(null);
+    }
 }
